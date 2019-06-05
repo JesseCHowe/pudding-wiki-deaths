055ea89f4def49119e5b014ffc81407d51fc1b5f
Jesse Howe <jchowe93@gmail.com>
2019-06-05 09:53:11 -0500

    First commit

---
+++ scripts/download-year-pages.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const mkdirp = require('mkdirp');
+const wiki = require('wikijs').default;
+
+const years = ['2015','2016','2017','2018'];
+const outputDir = "./output/year-pages";
+
+function download(year) {
+    wiki()
+    .page(year)
+	.then(page => page.html())
+    .then(response => {
+        fs.writeFileSync(`${outputDir}/${year}.html`, response);
+    });
+}
+
+mkdirp(outputDir);
+years.forEach(download)
+
+/*wiki()
+.page('2018')
+.then(page => page.html())
+.then(response => {
+    console.log(response);
+});*/
